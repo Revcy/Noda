06a30019df9ac324bd80ca6cccb67150864c8f79
Revcy <63965069+Revcy@users.noreply.github.com>
2022-07-05 15:24:25 +0300

    changed round

--- bot.py
+++ bot.py
@@ -26,7 +26,6 @@ def userinfo_report_function(message):
     api = requests.get('http://api.warface.ru/user/stat/', params=parameters)
 
     wfinfo = api.json()
-    # print(wfinfo)
     
     if api:
         wf_user_id = wfinfo['user_id']
@@ -53,7 +52,7 @@ def userinfo_report_function(message):
         wf_pvp_lose = wfinfo['pvp_lost']
         wf_pvp_all = wfinfo['pvp_all']
 
-        result = f'Username: {wf_nickname}\nUser ID: {wf_user_id}\n\nUser level: {wf_rank}\nUser experience: {wf_exp} exp\nUser playtime: {round(wf_playtime_seconds / 36000, 2)} hours\n\nPVP Information:\n\nAll kills - {wf_pvp_all_kills}\nEnemy kills - {wf_pvp_enemy_kills}\nFriendly kills - {wf_pvp_friendly_kills}\nDeaths - {wf_pvp_death}\nK/D - {wf_pvp_frags_kd}\nFavorite class - {wf_favorite_pvp_class}\nGames - {wf_pvp_all}\nWins - {wf_pvp_wins}\nLoses - {wf_pvp_lose}\n\nPVE Information:\n\nAll kills - {wf_pve_all_kills}\nEnemy kills - {wf_pve_enemy_kills}\nFriendly kills - {wf_pve_friendly_kills}\nDeaths - {wf_pve_death}\nK/D - {wf_pve_frags_kd}\nFavorite class - {wf_favorite_pve_class}\nGames - {wf_pve_all}\nWins - {wf_pve_wins}\nLoses - {wf_pve_lose}'
+        result = f'Username: {wf_nickname}\nUser ID: {wf_user_id}\n\nUser level: {wf_rank}\nUser experience: {wf_exp} exp\nUser playtime: {round(wf_playtime_seconds / 36000, 1)} hours\n\nPVP Information:\n\nAll kills - {wf_pvp_all_kills}\nEnemy kills - {wf_pvp_enemy_kills}\nFriendly kills - {wf_pvp_friendly_kills}\nDeaths - {wf_pvp_death}\nK/D - {wf_pvp_frags_kd}\nFavorite class - {wf_favorite_pvp_class}\nGames - {wf_pvp_all}\nWins - {wf_pvp_wins}\nLoses - {wf_pvp_lose}\n\nPVE Information:\n\nAll kills - {wf_pve_all_kills}\nEnemy kills - {wf_pve_enemy_kills}\nFriendly kills - {wf_pve_friendly_kills}\nDeaths - {wf_pve_death}\nK/D - {wf_pve_frags_kd}\nFavorite class - {wf_favorite_pve_class}\nGames - {wf_pve_all}\nWins - {wf_pve_wins}\nLoses - {wf_pve_lose}'
     else:
         wf_error_code = wfinfo['code']
 
