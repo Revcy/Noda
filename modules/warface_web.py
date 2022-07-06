7ec96118c7187ae3be7b3dd40708330e100d4a4d
Revcy <63965069+Revcy@users.noreply.github.com>
2022-07-06 22:19:34 +0300

    Cleared commended code

--- modules/warface_web.py
+++ modules/warface_web.py
@@ -35,13 +35,9 @@ def warface_request_user(username):
         wf_pve_all = wfinfo['pve_all']                          # 22
 
         result = [wf_user_id, wf_nickname, wf_exp, wf_rank, wf_pvp_all_kills, wf_pvp_friendly_kills, wf_pvp_enemy_kills, wf_pvp_death, wf_pvp_frags_kd, wf_pve_all_kills, wf_pve_friendly_kills, wf_pve_enemy_kills, wf_pve_death, wf_pve_frags_kd, wf_playtime_seconds, wf_favorite_pvp_class, wf_favorite_pve_class, wf_pvp_wins, wf_pvp_lose, wf_pvp_all, wf_pve_wins, wf_pve_lose, wf_pve_all]
-
-        # result = f'Username: {wf_nickname}\nUser ID: {wf_user_id}\n\nUser level: {wf_rank}\nUser experience: {wf_exp} exp\nUser playtime: {round(wf_playtime_seconds / 36000, 1)} hours\n\nPVP Information:\n\nAll kills - {wf_pvp_all_kills}\nEnemy kills - {wf_pvp_enemy_kills}\nFriendly kills - {wf_pvp_friendly_kills}\nDeaths - {wf_pvp_death}\nK/D - {wf_pvp_frags_kd}\nFavorite class - {wf_favorite_pvp_class}\nGames - {wf_pvp_all}\nWins - {wf_pvp_wins}\nLoses - {wf_pvp_lose}\n\nPVE Information:\n\nAll kills - {wf_pve_all_kills}\nEnemy kills - {wf_pve_enemy_kills}\nFriendly kills - {wf_pve_friendly_kills}\nDeaths - {wf_pve_death}\nK/D - {wf_pve_frags_kd}\nFavorite class - {wf_favorite_pve_class}\nGames - {wf_pve_all}\nWins - {wf_pve_wins}\nLoses - {wf_pve_lose}'
     else:
         wf_error_code = wfinfo['code']
 
         result = [None, wf_error_code]
-
-        # result = f"User not found or site don't response \nCode: {wf_error_code}"
 
     return result
